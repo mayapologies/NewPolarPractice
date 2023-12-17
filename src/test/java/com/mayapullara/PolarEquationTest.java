40f55a4ce50563fb0bb11853739e8fada12c0f61
Maya Pullara <mayapullara@gmail.com>
2023-12-16 16:54:30 -0800

    Added test for converting equation parameters to string

--- src/test/java/com/mayapullara/PolarEquationTest.java
+++ src/test/java/com/mayapullara/PolarEquationTest.java
@@ -7,20 +7,26 @@ import static org.junit.Assert.assertEquals;
 
 public class PolarEquationTest {
     @Test
-    public void testZero() {
+    public void testOne() {
         PolarEquation pe = new PolarEquation(1, 0, 0, 0);
         assertEquals(0, pe.getR(1), 0.000001);
     }
 
     @Test
-    public void testOne() {
+    public void testOneA() {
         PolarEquation pe = new PolarEquation(1, 1, 0, 0);
         assertEquals(1, pe.getR(1), 0.000001);
     }
 
     @Test
-    public void testTwo() {
+    public void testOneB() {
         PolarEquation pe = new PolarEquation(1, 0, 2, 0);
         assertEquals(1, pe.getR(Math.PI/6), 0.000001);
+    }
+
+    @Test
+    public void testString() {
+        PolarEquation pe = new PolarEquation(3, 1, 2, 4);
+        assertEquals("r = 1 * sin(4 * theta)", pe.toString());
     }
 }
